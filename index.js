d0ae1bf9bb4da6bcea598a534db274dd3e17480e
Sevillee <sevillee13@gmail.com>
2022-12-19 18:12:05 +0200

    l, ll;

--- index.js
+++ index.js
@@ -2,7 +2,31 @@ function check() {
     let personName = document.getElementById('personName');
     let userSurname = document.getElementById('userSurname');
 
+    let userMail = document.getElementById('userEmail');
+
+    let password = document.getElementById('psw');
+    let passwordRep = document.getElementById('psw-repeat');
+
+
+    document.getElementById('errorMessageName').innerHTML='';
+    document.getElementById('errorMessageSurname').innerHTML='';
+    document.getElementById('errorMessageEmail').innerHTML='';
+    document.getElementById('errorMessagePsw').innerHTML='';
+
     if(personName.value == ''){
-        document.getElementById('errorMessage').innerHTML+="Введите Ваше имя<br>";
+        document.getElementById('errorMessageName').innerHTML+="Введите Ваше имя<br>";
     }
+
+    if(userSurname.value == ''){
+        document.getElementById('errorMessageSurname').innerHTML+="Введите Вашу фамилию<br>";
+    }
+
+    if(userMail.value == ''){
+        document.getElementById('errorMessageEmail').innerHTML+="Введите Ваш емейл<br>";
+    }
+
+    if(password.value == ''){
+        document.getElementById('errorMessagePsw').innerHTML+="Поле не заполнено<br>";
+    }
+    
 }
